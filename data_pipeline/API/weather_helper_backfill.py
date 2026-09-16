4d6961ef847296a5da002fc342fbe544637203c0
squabzilla <55065342+squabzilla@users.noreply.github.com>
2026-09-15 23:05:19 -0600

    Refactored backfill argument to have separate fetch-single-year-logic, and loop-through-all-years logic. This way, I can call fetch-single-yeaer-logic portion separately in the case that I want to update the records for one year.

--- data_pipeline/API/weather_helper_backfill.py
+++ data_pipeline/API/weather_helper_backfill.py
@@ -42,6 +42,7 @@ import logging
 
 # custom libraries!
 from data_pipeline.helper.helper_timezones import AB_TIME
+from data_pipeline.helper.helper_SQL_tables import START_YEAR
 from data_pipeline.helper.helper_API_errors import APITimeoutError, APIConnectError, APIResponseError, \
     APIZeroCountError, APICountMismatchError, DataUniquenessConstraintViolation, DBError, DataPipelineError
 from data_pipeline.helper.helper_PSQL_config import default_SQL_engine
@@ -58,7 +59,81 @@ logging.getLogger("httpx").setLevel(logging.WARNING) # STOP LOGGING EVERY API CA
 
 
 ########################################################################################################################
-### section 1
+### section 1 - function to determine if a year is valid or not 
+### NOTE: includes `0` as valid input, which will be used to represent ALL years
+def valid_year(year: int) -> None:
+    if not type(year) is int:
+        msg = f"Error: TypeError: Expecting an integer; got {year} which is a {type(year)}."
+        raise TypeError(msg)
+    start_year = START_YEAR
+    current_year = datetime.now(AB_TIME).year # yeah I'm just being overly thorough with timezones lol
+    # however, my API call is grabbing everything where LOCAL_YEAR = passed_year, so I want current_year in AB time lol
+    stop_year = current_year + 1 # stop when we reach this year, BUT DO NOT PROCESS THIS YEAR
+    valid_years = range(start_year, stop_year)
+    #return(year in valid_years) # the statement `year in valid_years` will eval. to True or False
+    if year not in (0, valid_years):
+        msg = f"Error: ValueError: valid years are 0, or from {start_year} to {current_year} (inclusive). {year} not within that set."
+        raise ValueError(msg)
+
+
+
+########################################################################################################################
+### section 2
+"""
+returns `year` value if single year failed
+returns `0` otherwise
+"""
+
+def backfill_single_year(year: int, MSC_GeoMet_weather_by_year: Callable[[int], gpd.GeoDataFrame],
+                         main_table_name: str, staging_table_name: str, datetimecol: str,
+                         main_table_unique_constraint_name: str, staging_table_unique_constraint_name: str,
+                         dtype_dictionary: dict) -> int:
+    start_year = START_YEAR
+    try:
+        gdf = MSC_GeoMet_weather_by_year(year)
+    except DataPipelineError: # NOTE: API errors logged by function that actually makes raw API calls
+        if year == start_year:
+            msg = f"Error: DataPipelineError: Cannot retrieve data for start year {year}; aborting backfill."
+            raise DataPipelineError(msg)
+        return year
+    except Exception as e:
+        msg = f"Unexpected error while backfilling daily-weather-records: {e}"
+        logger.critical(msg, exc_info=True)
+        raise Exception(msg)
+    
+    # defining variables for database updating - putting multiple declaraions in one line because theres a lot lol
+    gdf = gdf; engine = default_SQL_engine(); unique_column = datetimecol
+    main_table_name=main_table_name; staging_table_name=staging_table_name; dtype_dictionary=dtype_dictionary
+    main_table_unique_constraint_name=main_table_unique_constraint_name
+    staging_table_unique_constraint_name=staging_table_unique_constraint_name
+    
+
+    if not year == start_year:
+        try: # we want to update our table with new records if it's NOT the start year
+            # because this is the backfill, we want to overwrite any records
+            add_new_records_to_table(gdf=gdf, engine=engine, main_table_name=main_table_name,
+                                        staging_table_name=staging_table_name, unique_column=unique_column,
+                                        staging_table_unique_constraint_name=staging_table_unique_constraint_name,
+                                        dtype_dictionary=dtype_dictionary, overwrite=True)
+        except:
+            logger.exception(f"Warning: DBError: DB write failed for {year}")
+            return year
+
+    else:
+        try: # For first year only: Write gdf to main table, overwriting old results if any
+            export_as_new_table(gdf=gdf, engine=engine, main_table_name=main_table_name, dtype_dictionary=dtype_dictionary,
+                                main_table_unique_constraint_name=main_table_unique_constraint_name, unique_column=unique_column)
+        except DBError:
+            error_message = f"Error: DBError: Cannot create table for start year {year}; aborting backfill."
+            logger.critical(error_message, exc_info=True)
+            raise DBError(error_message) # this will end things script - but somethings gone HORRIBLY wrong if db connection fails here...
+    
+    return 0 # if we get here, return 0 suggesting status is good
+
+
+
+########################################################################################################################
+### section 3
 ### Function to backfill years of weather data, from 1956 (first date in watermain break data) to current.
 ### Takes, as input, a `fetch_year` function: 
 ### this function takes an integer year as input, and returns a geodataframe
@@ -75,30 +150,21 @@ def backfill_weather_years(MSC_GeoMet_weather_by_year: Callable[[int], gpd.GeoDa
                            main_table_unique_constraint_name: str, staging_table_unique_constraint_name: str,
                            dtype_dictionary: dict, progress_bar_prefix: str) -> int:
 
-    start_year = 1956 # first date in watermain break data
+    
 
-    EXIT_CODE = 0
-    # NOTE: I want to manually define and return the exit code,
-    # so that the scripts that call this one can end themselves with that code
-    # that way, if there's an error, the script can exit with a non-zero error code
-    # and then when bash runs it, it can identify that the error code is not-zero, meaning THERE BE A PROBLEM GUYS
-    # so if I hit any of my exceptions, the exit code becomes 1
-
-    TESTING_CODE = False
-    if TESTING_CODE == True: start_year = 2025
-    if start_year == 2025:
-        warning_message = (
-            "\n##################################################\nWARNING: START YEAR IS 2025 FOR TEST\n##################################################\n"
-        )
-        print(warning_message)
-        logger.info(warning_message)
-
-    #current_year = date.today().year
-    current_year = datetime.now(AB_TIME).year # yeah I'm just being overly thorough with timezones lol
-    # however, my API call is grabbing everything where LOCAL_YEAR = passed_year, so I want current_year in AB time lol
+    # local variables
+    EXIT_CODE = 0 # exit code with 0 marking success, 1 marks that there was a problem
+    start_year = START_YEAR # 1956 # first date in watermain break data
+    current_year = datetime.now(AB_TIME).year # being overly thorough with timezones but API grabs things where `LOCAL_YEAR = passed_year` so want AB time
     stop_year = current_year + 1 # stop when we reach this year, BUT DO NOT PROCESS THIS YEAR
-
     failed_years = []
+
+    # adding some logic just to be used during testing
+    TESTING_CODE = True
+    if TESTING_CODE == True:
+        start_year = 2025
+        warning_message = ("\n#############################################\nWARNING: START YEAR IS 2025 FOR TEST\n#############################################\n")
+        logger.info(warning_message)
 
     # progress bar variables
     total_iterations = stop_year - start_year
@@ -106,66 +172,29 @@ def backfill_weather_years(MSC_GeoMet_weather_by_year: Callable[[int], gpd.GeoDa
     update_progress_bar(iteration=progress_bar_count, total=total_iterations, prefix=progress_bar_prefix) # first iteration is 0
 
 
+    ###################### starting yearly backfill now
+
     for year in range(start_year, stop_year):
         # print(f"year: {year}")
         
-        logger.info(f"{progress_bar_prefix} for year: {year}")
+        logger.info(f"{progress_bar_prefix} for year: {year}", extra={"console_exclude": True})
         # since I'm already passing a nice starting prefix explaining what I'm doing lol
 
-        try:
-            gdf = MSC_GeoMet_weather_by_year(year)
-        except DataPipelineError:
-            EXIT_CODE = 1
-            # this catches every subset of datapipeline error, so I don't need to specify each one!
+        # run backfill single year, get exit code
+        backfillExitCode = backfill_single_year(\
+            start_year=start_year, year=year, MSC_GeoMet_weather_by_year=MSC_GeoMet_weather_by_year,
+            main_table_name=main_table_name, staging_table_name=staging_table_name, datetimecol=datetimecol,
+            main_table_unique_constraint_name=main_table_unique_constraint_name,
+            staging_table_unique_constraint_name=staging_table_unique_constraint_name,
+            dtype_dictionary=dtype_dictionary)
+        # exit code will be the year if there was a problem; append the year to failed_years, set EXIT_CODE to 1
+        if backfillExitCode != 0:
             failed_years.append(year)
-            if year == start_year: break
-            continue # skip to next iteration if we don't break
-        except Exception as e:
             EXIT_CODE = 1
-            msg = f"Unexpected error while backfilling daily-weather-records: {e}"
-            logger.critical(msg, exc_info=True)
-            break
 
-        # define variables for database updating
-        gdf = gdf
-        engine = default_SQL_engine()
-        main_table_name = main_table_name
-        staging_table_name = staging_table_name
-        unique_column = datetimecol
-        main_table_unique_constraint_name = main_table_unique_constraint_name
-        staging_table_unique_constraint_name = staging_table_unique_constraint_name
-        dtype_dictionary = dtype_dictionary
-
-        if not year == start_year:
-            # we want to update our table with new records if it's NOT the start year
-            # def add_new_records_to_table(gdf, engine, main_table_name, staging_table_name,
-                                         # unique_column, staging_table_unique_constraint_name,
-                                         # dtype_dictionary)
-            try:
-                # because this is the backfill, we want to overwrite any records
-                add_new_records_to_table(gdf=gdf, engine=engine, main_table_name=main_table_name,
-                                         staging_table_name=staging_table_name, unique_column=unique_column,
-                                         staging_table_unique_constraint_name=staging_table_unique_constraint_name,
-                                         dtype_dictionary=dtype_dictionary, overwrite=True)
-            except:
-                logger.exception(f"Warning: DBError: DB write failed for {year}")
-                failed_years.append(year)
-
-        else:
-            # Write gdf to main table, overwriting old results if any
-            # def export_as_new_table(gdf, engine, main_table_name, dtype_dictionary, main_table_unique_constraint_name, unique_column):
-            try:
-                export_as_new_table(gdf=gdf, engine=engine, main_table_name=main_table_name, dtype_dictionary=dtype_dictionary,
-                                    main_table_unique_constraint_name=main_table_unique_constraint_name, unique_column=unique_column)
-            except DBError:
-                error_message = f"Error: DBError: Cannot create table for start year {year}; aborting backfill."
-                logger.critical(error_message, exc_info=True)
-                raise DBError(error_message) # this will end things script - but somethings gone HORRIBLY wrong if db connection fails here...
-
+        # update progress bar AFTER iteration of loop is done...
         progress_bar_count += 1
-        # NOTE: let's update progress bar AFTER iteration of loop is done...
         update_progress_bar(iteration=progress_bar_count, total=total_iterations, prefix=progress_bar_prefix)
-        # NOTE: progress bar output isn't recorded by Python log! Woo!
 
     logger.info(f"Number of failed years: {len(failed_years)}.")
     if len(failed_years) > 0:
